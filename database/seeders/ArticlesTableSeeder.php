bcb9333e9614959724bb9231c481c69c985dd110
VZ <131747053+VaZoi@users.noreply.github.com>
2024-03-01 12:38:43 +0100

    s

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Article;
+
+class ArticlesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        Article::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            Article::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+            ]);
+        }
+    }
+}
